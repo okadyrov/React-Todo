786da98c84263e8adcb22ee91fbe8e8c9f5a6377
Oleg Kadyrov <abathojalyk@gmail.com>
2020-02-16 21:04:07 -0600

    commit_1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search"/>
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesom App</li>
+        </ul>
+    </div>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    ReactDOM.render(el,
+        document.getElementById('root'));
